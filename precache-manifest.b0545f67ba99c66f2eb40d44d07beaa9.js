8f5c5a1c1d89390f656ad8d609fd27e34cb487af
rinse <rinse@neko2.net>
2020-11-22 23:15:19 +0900

    Updates

---
+++ precache-manifest.b0545f67ba99c66f2eb40d44d07beaa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3e961676aa954a2e2f84f14a7fce45",
+    "url": "/seiheki-werewolf-front/index.html"
+  },
+  {
+    "revision": "3dfe94f2c6b08ee2fec4",
+    "url": "/seiheki-werewolf-front/static/js/2.7786e749.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/seiheki-werewolf-front/static/js/2.7786e749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111ba6ceaa086447ce70",
+    "url": "/seiheki-werewolf-front/static/js/main.0dd65d5c.chunk.js"
+  },
+  {
+    "revision": "d61459cc58c27d587fa8",
+    "url": "/seiheki-werewolf-front/static/js/runtime-main.d5ed7b98.js"
+  }
+]);
