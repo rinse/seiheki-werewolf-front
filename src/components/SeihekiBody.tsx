0411f568427d5a1c1201d449e8653307e84cf9ad
rinse <rinse@neko2.net>
2020-11-23 20:59:33 +0900

    remove unused import

--- src/components/SeihekiBody.tsx
+++ src/components/SeihekiBody.tsx
@@ -1,4 +1,4 @@
-import React, {MouseEventHandler} from "react";
+import React from "react";
 import Content from "./Content";
 import SeihekiFooter from "./SeihekiFooter";
 import Seiheki from "../types/Seiheki";
