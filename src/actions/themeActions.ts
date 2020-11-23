4cea9e7d90d3def0f6e2433e89d80b0cf040b97c
rinse <rinse@neko2.net>
2020-11-23 19:22:39 +0900

    themeActions.ts

--- src/actions/themeActions.ts
+++ src/actions/themeActions.ts
@@ -3,8 +3,9 @@
  * @returns {{payload: Promise, type: string}}
  */
 import {FETCH_THEME} from "../actionTypes";
+import Client from "../api/v3/Client";
 
-export function fetchTheme(client) {
+export function fetchTheme(client: Client) {
     return {
         type: FETCH_THEME,
         payload: client.getCards(1)
