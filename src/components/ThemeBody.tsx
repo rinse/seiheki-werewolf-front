649cdac6fb477546c781f4e8e0ea9c6daf6727f4
rinse <rinse@neko2.net>
2020-11-26 01:22:50 +0900

    replace Content with Typography

--- src/components/ThemeBody.tsx
+++ src/components/ThemeBody.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Content from "./Content";
 import QuestionCircle from "./icons/QuestionCircle"
 import ThemeFooter from "./ThemeFooter";
 import Seiheki from "../types/Seiheki";
+import {createStyles, Theme, Typography} from "@material-ui/core";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 interface Properties {
     value: Seiheki;
@@ -12,24 +13,22 @@ interface Properties {
     onShuffleClick: (e: React.MouseEvent<Element, MouseEvent>) => void;
 }
 
-/**
- * @param props.value
- * @param props.onCommentClick
- * @param props.onUpvotesClick
- * @returns {JSX.Element}
- * @constructor
- */
+export const useStyles = makeStyles((theme: Theme) => createStyles({
+    typography: {
+        whiteSpace: "pre-line",
+    }
+}));
+
 export default function ThemeBody(props: Properties) {
     const seiheki = props.value;
+    const classes = useStyles();
     return (
         <div className="border">
             <div className="m-4 d-flex flex-row">
                 <div className="mr-4" onClick={e => props.onCommentClick(e)} style={{color: "#6c757d"}}>
                     <QuestionCircle />
                 </div>
-                <div onClick={e => props.onCommentClick(e)}>
-                    <Content>{seiheki.content}</Content>
-                </div>
+                <Typography className={classes.typography}>{seiheki.content}</Typography>
             </div>
             <ThemeFooter value={seiheki}
                          onUpvotesClick={e => props.onUpvotesClick(e)}
