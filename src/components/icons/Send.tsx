50a6f8dc496ada5a8887655a8d022d275be183b7
rinse <rinse@neko2.net>
2020-11-26 01:11:17 +0900

    remove unused icons

--- src/components/icons/Send.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Send() {
-    return (
-        <svg width="2em" height="2em" viewBox="0 0 24 24">
-            <path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z" />
-        </svg>
-    );
-}
