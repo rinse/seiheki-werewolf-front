ae99099e6f906315e7465c13e40fb460c873c77c
rinse <rinse@neko2.net>
2020-11-26 01:53:44 +0900

    ThemeBody

--- src/components/ThemeBody.tsx
+++ src/components/ThemeBody.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import QuestionCircle from "./icons/QuestionCircle"
 import Seiheki from "../types/Seiheki";
 import {createStyles, makeStyles, Theme, Typography} from "@material-ui/core";
 import SeihekiFooter from "./SeihekiFooter";
+import {Help} from "@material-ui/icons";
+import grey from "@material-ui/core/colors/grey";
 
 interface Properties {
     value: Seiheki;
@@ -13,7 +14,12 @@ interface Properties {
 export const useStyles = makeStyles((theme: Theme) => createStyles({
     typography: {
         whiteSpace: "pre-line",
-    }
+    },
+    avatar: {
+        color: grey[600],
+        width: theme.spacing(8),
+        height: theme.spacing(8),
+    },
 }));
 
 export default function ThemeBody(props: Properties) {
@@ -22,8 +28,8 @@ export default function ThemeBody(props: Properties) {
     return (
         <div className="border">
             <div className="m-4 d-flex flex-row">
-                <div className="mr-4" onClick={e => props.onCommentClick(e)} style={{color: "#6c757d"}}>
-                    <QuestionCircle />
+                <div className="mr-4">
+                    <Help className={classes.avatar} fontSize="large" />
                 </div>
                 <Typography className={classes.typography}>{seiheki.content}</Typography>
             </div>
